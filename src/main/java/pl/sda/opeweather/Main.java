9110c2aaecd3325647588b49ef0222d698f4c11b
Bartlomiej Lazarek <bartek.lazarek@gmail.com>
2019-02-10 11:58:13 +0100

    addes weatherService class and 2 methods

--- src/main/java/pl/sda/opeweather/Main.java
+++ src/main/java/pl/sda/opeweather/Main.java
@@ -14,15 +14,13 @@ public class Main {
     public static void main(String[] args) {
 
 
-        URL jsonURL = null;
-        try {
-            jsonURL = new URL("http://api.apixu.com/v1/current.json?key=23f7a82df6764a3986590706191002&q=Bydgoszcz");
-            ObjectMapper objectMapper = new ObjectMapper();
-            Weather weather = objectMapper.readValue(jsonURL, Weather.class);
-            System.out.println(weather);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        WeatherService weatherService = new WeatherService(
+                "http://api.apixu.com/v1/current.json",
+                "23f7a82df6764a3986590706191002");
+        
+        weatherService.getCityWeatherFullInfo("Bydgoszcz");
+        weatherService.getCityWeatherOnlyTemp("Bydgoszcz");
+
 
 
 
---
+++ src/main/java/pl/sda/opeweather/WeatherService.java
@@ -0,0 +1,48 @@
+package pl.sda.opeweather;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import pl.sda.opeweather.model.Weather;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class WeatherService {
+
+    private String finalURL;
+    private String url;
+    private String apiKey;
+
+
+    public WeatherService(String url, String apiKey) {
+        this.url = url;
+        this.apiKey = apiKey;
+    }
+
+    public void getCityWeatherFullInfo(String city){
+        finalURL = url + "?key=" + apiKey + "&q=" + city;
+        URL jsonURL = null;
+        try {
+            jsonURL = new URL(finalURL);
+            ObjectMapper objectMapper = new ObjectMapper();
+            Weather weather = objectMapper.readValue(jsonURL, Weather.class);
+            System.out.println(weather);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getCityWeatherOnlyTemp(String city){
+        finalURL = url + "?key=" + apiKey + "&q=" + city;
+        URL jsonURL = null;
+        try {
+            jsonURL = new URL(finalURL);
+            ObjectMapper objectMapper = new ObjectMapper();
+            Weather weather = objectMapper.readValue(jsonURL, Weather.class);
+            System.out.println("Tempteratura w " + city + " wynosi: " + weather.getCurrent().getTemp_c() + " stopni");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
