6bcc1e48392b3917306323960979e3bdb30301a6
Bartlomiej Lazarek <bartek.lazarek@gmail.com>
2019-02-10 10:50:04 +0100

    added model classes

---
+++ src/main/java/pl/sda/opeweather/model/Location.java
@@ -0,0 +1,76 @@
+package pl.sda.opeweather.model;
+
+public class Location {
+    private String name;
+    private String region;
+    private String county;
+    private float lat;
+    private float lon;
+    private String tz_id;
+    private String localtime_epoch;
+    private String localtime;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public float getLon() {
+        return lon;
+    }
+
+    public void setLon(float lon) {
+        this.lon = lon;
+    }
+
+    public String getTz_id() {
+        return tz_id;
+    }
+
+    public void setTz_id(String tz_id) {
+        this.tz_id = tz_id;
+    }
+
+    public String getLocaltime_epoch() {
+        return localtime_epoch;
+    }
+
+    public void setLocaltime_epoch(String localtime_epoch) {
+        this.localtime_epoch = localtime_epoch;
+    }
+
+    public String getLocaltime() {
+        return localtime;
+    }
+
+    public void setLocaltime(String localtime) {
+        this.localtime = localtime;
+    }
+}
---
+++ src/main/java/pl/sda/opeweather/model/Weather.java
@@ -0,0 +1,6 @@
+package pl.sda.opeweather.model;
+
+public class Weather {
+    private Location location;
+    private Current current;
+}
